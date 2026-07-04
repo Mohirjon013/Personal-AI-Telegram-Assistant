49dfa2d2883913606c42f0a650728107d73d8471
Mohirjon013 <mohirjon26@gmail.com>
2026-07-04 20:29:13 +0500

    Module 2 completed: PostgreSQL integration working

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import pool from "./database.js";
+
+
+async function testConnection() {
+    try{
+        const result = await pool.query("SELECT NOW()")
+
+        console.log("✅ Database connected: ", result.rows[0]);
+    }
+    catch(error){
+        console.log("❌ Database connection error:", error.message);
+    }
+}
+
 console.log("🚀 Mohirjon AI is starting...");
+
+testConnection();
